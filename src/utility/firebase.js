24b5be7492e57fdb287473f90908ae424960e45b
yohanis tasfa <yohanistesfa457@gmail.com>
2025-11-27 21:43:08 +0300

    firebase setup

---
+++ src/utility/firebase.js
@@ -0,0 +1,22 @@
+// Import Firebase functions
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXcBKpaiXyAHapyO59lu6NHS1sXf8LYwQ",
+  authDomain: "clone-72e28.firebaseapp.com",
+  projectId: "clone-72e28",
+  storageBucket: "clone-72e28.firebasestorage.app",
+  messagingSenderId: "116186194904",
+  appId: "1:116186194904:web:2c61da9148da43338a88ad",
+  measurementId: "G-K4TJFHMEBT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services (v9 modular)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
