9a6ff0c8774f41353743b37293af8ff8e9ad581e
yohanis tasfa <yohanistesfa457@gmail.com>
2025-11-29 01:41:00 +0300

    backend payment setup

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+// const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const stripe = require("stripe")(process.env.SSTRIPE_KEY);
+
+const app = express();
+
+app.use(cors({origin: true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  if (total >0) {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  console.log(paymentIntent);
+  res.status(201).json(paymentIntent);
+} else {
+  res.status(400).json({message: "Invalid amount"});
+}
+});
+
+exports.api = onRequest(app);
+
+
+
+
+
+
+// setGlobalOptions({ maxInstances: 10 });
+
+
